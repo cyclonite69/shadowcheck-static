dd92a9d4638eeec38d8bf7d22ba451ad2cee9cb6
cyclonite69 <cyclonite69@proton.me>
2026-01-23 06:45:37 -0500

    refactor: centralize credentials initialization

---
+++ src/utils/credentialsInit.js
@@ -0,0 +1,20 @@
+/**
+ * Credentials initialization helpers.
+ */
+
+/**
+ * Validate and initialize credentials manager.
+ * @returns {Promise<object>} Credentials manager instance
+ */
+async function initializeCredentials() {
+  const { validateSecrets } = require('./validateSecrets');
+  const secretsManager = require('../services/secretsManager');
+
+  await validateSecrets();
+
+  return secretsManager;
+}
+
+module.exports = {
+  initializeCredentials,
+};
