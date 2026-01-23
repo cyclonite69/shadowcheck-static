57b43f7afaf0be1cc3933e641945c1725533275c
cyclonite69 <cyclonite69@proton.me>
2026-01-23 02:43:23 -0500

    refactor: extract shutdown handlers

--- server.js
+++ server.js
@@ -173,19 +173,8 @@ delete process.env.PGUSER;
     });
 
     // Graceful shutdown
-    process.on('SIGTERM', async () => {
-      logger.info('SIGTERM received, closing server gracefully...');
-      const BackgroundJobsService = require('./src/services/backgroundJobsService');
-      BackgroundJobsService.shutdown();
-      await pool.end();
-      process.exit(0);
-    });
-
-    process.on('SIGINT', async () => {
-      logger.info('SIGINT received, closing server gracefully...');
-      await pool.end();
-      process.exit(0);
-    });
+    const { registerShutdownHandlers } = require('./src/utils/shutdownHandlers');
+    registerShutdownHandlers({ logger, pool });
   } catch (err) {
     console.error(err); // PRINT STACK TRACE
     const logger = require('./src/logging/logger');
