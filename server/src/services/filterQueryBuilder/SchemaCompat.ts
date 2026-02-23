87ee2844da5cc1f1f49eb32f374e27db4f22fea9
Cyclonite01 <cyclonite01@users.noreply.github.com>
2026-02-23 05:36:49 -0500

    fix(types): widen SchemaCompat function signatures to accept string aliases

--- server/src/services/filterQueryBuilder/SchemaCompat.ts
+++ server/src/services/filterQueryBuilder/SchemaCompat.ts
@@ -6,22 +6,22 @@ export const SCHEMA_ALIASES = {
 } as const;
 
 export const FIELD_EXPRESSIONS = {
-  networkBssid: (alias = SCHEMA_ALIASES.network) => `UPPER(${alias}.bssid)`,
-  manufacturerName: (alias = SCHEMA_ALIASES.manufacturer) =>
+  networkBssid: (alias: string = SCHEMA_ALIASES.network) => `UPPER(${alias}.bssid)`,
+  manufacturerName: (alias: string = SCHEMA_ALIASES.manufacturer) =>
     `COALESCE(to_jsonb(${alias})->>'organization_name', to_jsonb(${alias})->>'manufacturer', to_jsonb(${alias})->>'manufacturer_name')`,
-  manufacturerAddress: (alias = SCHEMA_ALIASES.manufacturer) =>
+  manufacturerAddress: (alias: string = SCHEMA_ALIASES.manufacturer) =>
     `COALESCE(to_jsonb(${alias})->>'organization_address', to_jsonb(${alias})->>'address')`,
-  threatTag: (alias = SCHEMA_ALIASES.networkTags) =>
+  threatTag: (alias: string = SCHEMA_ALIASES.networkTags) =>
     `COALESCE(to_jsonb(${alias})->>'threat_tag', to_jsonb(${alias})->>'tag_type')`,
-  threatTagLowercase: (alias = SCHEMA_ALIASES.networkTags) =>
+  threatTagLowercase: (alias: string = SCHEMA_ALIASES.networkTags) =>
     `LOWER(COALESCE(to_jsonb(${alias})->>'threat_tag', to_jsonb(${alias})->>'tag_type'))`,
-  observationLat: (alias = SCHEMA_ALIASES.observation) =>
+  observationLat: (alias: string = SCHEMA_ALIASES.observation) =>
     `COALESCE(${alias}.lat, ST_Y(${alias}.geom::geometry))`,
-  observationLon: (alias = SCHEMA_ALIASES.observation) =>
+  observationLon: (alias: string = SCHEMA_ALIASES.observation) =>
     `COALESCE(${alias}.lon, ST_X(${alias}.geom::geometry))`,
 } as const;
 
 export const NULL_SAFE_COMPARISONS = {
-  isIgnored: (alias = SCHEMA_ALIASES.networkTags) =>
+  isIgnored: (alias: string = SCHEMA_ALIASES.networkTags) =>
     `COALESCE((to_jsonb(${alias})->>'is_ignored')::boolean, FALSE)`,
 } as const;
